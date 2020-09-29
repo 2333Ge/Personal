520007ecd0ae08aaac21e6785bd7b739ec9bbea6
yangchaohao <ych.class@qq.com>
2020-09-29 13:27:12 +0800

    commitMsg

---
+++ bin/push.sh
@@ -0,0 +1,14 @@
+echo "\n"
+echo "== auto push start "
+echo "\n"
+git add .
+git status
+commitMsg='atuo commit'
+if [ -n $1]; then
+    commitMsg=$1
+fi
+git commit -m commitMsg
+git push
+echo "\b"
+echo "<=====  auto push end  "
+echo "\n"
