ae56213e7d2390ab989a52e62f25b208893b6937
yangchaohao <ych.class@qq.com>
2020-09-29 13:57:31 +0800

    shell

--- bin/push.sh
+++ bin/push.sh
@@ -1,14 +1,14 @@
 echo "\n"
-echo "== push start "
+echo "push start"
 echo "\n"
 git add .
 git status
 commitMsg='atuo commit'
-if [ -n $1 ]; then
+if [ -n "$1" ]; then
     commitMsg=$1
 fi
-git commit -m $commitMsg
+git commit -m "$commitMsg"
 git push
 echo "\n"
-echo "<===== push end  "
+echo "push end"
 echo "\n"
